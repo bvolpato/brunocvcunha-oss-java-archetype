ba42176e7b01b19eccf4223d45fbe44a0282c74d
Bruno Volpato <brunocvcunha@gmail.com>
2017-02-05 00:08:47 -0800

    few improvements / bump dependencies

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016 Bruno Candido Volpato da Cunha (brunocvcunha@gmail.com)
+ * Copyright (C) 2017 Bruno Candido Volpato da Cunha (brunocvcunha@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package ${package};
+
 public class App {
     
     //TODO: build app!
